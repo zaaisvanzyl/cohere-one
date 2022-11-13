32783f07dbeadee8656645fb9bb531bd3472768b
zaaisvanzyl <vanzylzaais@gmail.com>
2022-11-13 13:02:06 -0500

    if statements for potential predictions

--- server/controllers/baseController.js
+++ server/controllers/baseController.js
@@ -1,30 +1,27 @@
 const { openAIGPT3, openAIDALLE2 } = require('./openAIController')
+const { triageData } = require('./data');
 
 
 const cohere = require('cohere-ai');
 cohere.init(process.env.COHERE_API_KEY);
 
 const cohereClassification = async (req, res) => {
-    const response = await cohere.classify({
-        model: 'large',
+    const response = await cohere.classify({ 
+        model: 'large', 
         inputs: [req.body.inputs],
-        examples: [{"text": "Write a python function that adds two numbers", "label": "Codex"}, {"text": "Create an image of a dog wearing a space suit", "label": "DALLE-2"}, {"text": "Classify the text from this chat conversation", "label": "Cohere"}, {"text": "Create a picture of a cowboy on mars", "label": "DALLE-2"}, {"text": "Create a Java method that checks two integers for equality", "label": "Codex"}, {"text": "Summarize this academic paper", "label": "GPT-3"}, {"text": "Create a SQL request to find all users who live in California and have over 1000 credits", "label": "GPT-3"}, {"text": "Write an outline for an essay about Ben Franklin", "label": "GPT-3"}, {"text": "Classify the sentiment of this tweet", "label": "Cohere"}, {"text": "Identify the toxic language in this chat", "label": "Cohere"}, {"text": "Write a Python function that prints \"Hello world\"", "label": "Codex"}, {"text": "Create an image of a monkey piloting a space ship", "label": "DALLE-2"}, {"text": "Create a video of a car driving", "label": "Other"}, {"text": "Text my mum \"Hello\"", "label": "Other"}, {"text": "Build a new internet", "label": "Other"}]
-    });
+        examples: triageData
+      }); 
 
     const prompt = req.body.inputs
     const prediction = response.body.classifications[0].prediction
     console.log("Prediction: " + prediction)
     const confidences = response.body.classifications[0].confidences
     console.log(confidences)
-    res.status(200).json(response.body.classifications)
 
     if (prediction === 'Cohere Generate') {
         res.status(200).json("Cohere (api not connected yet)")
     }
 
-    if (prediction === 'Cohere Embed') {
-        res.statu(200).json("Cohere (api not connected yet)")
-    }
 
     if (prediction === 'Cohere Classify') {
         res.status(200).json("Cohere (api not connected yet)")
@@ -33,17 +30,19 @@ const cohereClassification = async (req, res) => {
     if (prediction === 'GPT-3') {
         const openAIResponse = await openAIGPT3(prompt)
         console.log("Openai response: " + openAIResponse)
-        // res.status(200).json(openAIResponse)
+        res.status(200).json(openAIResponse)
     }
 
     if (prediction === 'Codex') {
-        // res.status(200).json("Codex (api not connected yet)")
+        res.status(200).json("Codex (api not connected yet)")
     }
 
     if (prediction === 'DALLE-2') {
-        const response = await openAIDALLE2(prompt)
-        console.log("Image url: " + response)
-        // res.status(200).json(response)
+        res.status(200).json("DALLE-2 (api not connected yet)")
+    }
+
+    if (prediction === 'Other') {
+        res.status(200).json("Other (did not match any apis)")
     }
 
 }
